4df8d1516848f8fee0167c35c602180b3e9b61ca
JagerMeister91 <46936284+JagerMeister91@users.noreply.github.com>
2019-02-05 15:37:48 -0500

    Update StudentArray.java

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
@@ -9,6 +9,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * A class to create an array of students and print their names
  * @author Ali Hassan, 2019
  * My first comment added after branch creation
+ * My second comment added after branch was pushed.
  */
 public class StudentArray 
 {
